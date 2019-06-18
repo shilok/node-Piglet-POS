fdcb5dcee81aa3adb60d3aa17a827cf50aa1cb41
shiloAWS <s.kohelet@gmail.com>
2019-06-18 18:21:19 +0300

    email test

--- routes/crud/customer.js
+++ routes/crud/customer.js
@@ -6,8 +6,16 @@ const knex = require('../../knex')
 
 
 
-const createCustomerEmail = (trx, email) => {
+const createEmail = (trx, email) => {
     return trx.insert({address: email}).into('Email')
+}
+
+const createCustomerEmail = (trx, customerID, emailID) => {
+    return trx.insert({customerID: customerID, emailID: emailID}).into('CustomerEmail')
+}
+
+const createCustomerPhone = (trx, phone) => {
+    return trx.insert({number: phone}).into('Phone')
 }
 
 router.post('/email', (req, res) => {
@@ -16,13 +24,13 @@ router.post('/email', (req, res) => {
     let customerID = 0
 
     knex.transaction((trx) => {
-        createCustomerEmail(trx, "test123@test.com")
-          .then((ids) => {
-            console.log(`ids - : ${ids}`)
+        createEmail(trx, "test123@test.com")
+          .then((emailID) => {
+            createCustomerEmail(trx, 2222, emailID)
             });
           })
       .then((inserts) => {
-        console.log(inserts.length + ' new books saved.');
+        console.log(inserts +  ' new books saved.');
       })
       .catch((error) => {
           res.send(error)
