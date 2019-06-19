664019180878ab12470798a4e9f42f79b4a2a889
shiloAWS <s.kohelet@gmail.com>
2019-06-19 15:34:47 +0300

    added credentials file

--- app.js
+++ app.js
@@ -3,18 +3,14 @@ const https = require('https')
 const fs = require('fs');
 
 const app = express()
+const PORT = process.env.PORT || 5000;
+
+const credentials = require('./credentials')
 
 app.use(express.json())
 
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/piglet-pos.com/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/piglet-pos.com/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/piglet-pos.com/chain.pem', 'utf8');
 
-const credentials = {
-	key: privateKey,
-	cert: certificate,
-	ca: ca
-};
+
 const httpsServer = https.createServer(credentials, app);
 
 
@@ -29,7 +25,6 @@ app.use('/api/testing', require('./routes/crud/employee'))
 
 
 
-const PORT = process.env.PORT || 5000;
 
 
 httpsServer.listen(PORT, () => {console.log(`HTTPS Server running on port ${PORT}`)})
