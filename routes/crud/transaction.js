bf1e079375f573c50366f2ca19ebd13f8dcda071
shilok <s.kohelet@gmail.com>
2019-06-19 18:05:43 +0300

    added 'add Product to order' function

--- routes/crud/transaction.js
+++ routes/crud/transaction.js
@@ -13,5 +13,31 @@ router.post('/createOrder', (req, res) => {
     })
 })
 
+router.post('/addProductToOrder', (req, res) => {
+    const quantity = req.body.quantity
+    let quantityAvailable = false
+    const inventoryID = req.body.inventoryID
+    const productID = req.body.productID
+    knex.transaction(trx => {
+        return trx('InventoryProduct')
+                .first('quantity')
+                .where({inventoryID: inventoryID, productID: productID}).then((result)=>{
+                    console.log(result)
+                    if (result.quantity >= quantity){
+                        quantityAvailable = true
+                        return trx('InventoryProduct').decrement('quantity', quantity)
+                    }
+        }) 
+    }).then(()=>{
+        if (quantityAvailable){
+            res.status(201).send('Success')
+        }else{
+            res.status(202).send('Missing in Inventory')
+        }
+    }).catch(error => {
+        res.status(400).send(error)
+    })
+})
+
 
 module.exports = router
