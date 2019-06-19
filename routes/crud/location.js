0cd083fb85580477c9e1b955f77c1db54565c60e
shiloAWS <s.kohelet@gmail.com>
2019-06-19 15:26:35 +0300

    added locationID

--- routes/crud/location.js
+++ routes/crud/location.js
@@ -3,6 +3,21 @@ var router = express.Router()
 
 const knex = require('../../knex')
 
+const createStore = (store) => {
+    return knex('Store').insert(store)
+}
+
+const createInventory = (inventory) => {
+    return knex('Inventory').insert(inventory)
+}
+
+const createInventoryLocation = (inventoryLocation) => {
+    return knex('InventoryLocation').insert(inventoryLocation)
+}
+
+
+
+
 router.post('/createStore', (req, res) => {
 
     const store = req.body.store
