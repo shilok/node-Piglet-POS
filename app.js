fe49002f69bbc275116b34d9ead81b856e141f19
shilok <s.kohelet@gmail.com>
2019-08-13 00:07:13 +0300

    init Angular Client

--- app.js
+++ app.js
@@ -5,14 +5,25 @@ const app = require('./config/app')
 const PORT = process.env.PORT || 5000;
 const logger = require('./config/winston')
 const httpsServer = require('./config/httpServer')
-
+const path = require('path')
  
 app.use(logger.info)
-
 app.use(cors())
 app.use(passport.initialize())
 app.use(passport.session())
 require('./config/passport')(passport)
+
+
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.engine('html', require('ejs').renderFile)
+
+app.use(express.static(path.join(__dirname, 'views')))
+
+app.get('/', (req, res, next) => {
+  res.render('index.html')
+})
 
 app.use('/api/testing', require('./routes/crud/products'))
 app.use('/api/testing', require('./routes/crud/customer'))
@@ -24,11 +35,11 @@ app.use('/api/users', require('./routes/users'))
 
 
     
-  app.use(logger.errorWin)
+  // app.use(logger.errorWin)
 
-  app.use((err, req, res, next) => {
-    return res.status(500).json({ success: false, status: 'error'})
-  })
+  // app.use((err, req, res, next) => {
+  //   return res.status(500).json({ success: false, status: 'error'})
+  // })
 
 
 
